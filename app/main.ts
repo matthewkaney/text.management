6b71ed110032e11b88c95f41a55a6ea4c485bb51
Matthew <matthew@mindofmatthew.com>
2021-03-17 12:19:12 -0400

    Add key binding

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,18 @@
-import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
-import {StreamLanguage} from "@codemirror/stream-parser"
-import {haskell} from "@codemirror/legacy-modes/mode/haskell"
+import {keymap, KeyBinding} from "@codemirror/view";
+import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup";
+import {StreamLanguage} from "@codemirror/stream-parser";
+import {haskell} from "@codemirror/legacy-modes/mode/haskell";
 
 import {oneDark} from "./theme";
+
+let commands: KeyBinding[] = [
+  {key: "Shift-Enter", run: () => { console.log("RUN SOMETHING!"); return true}}
+];
 
 window.addEventListener("load", () => {
   let editor = new EditorView({
     state: EditorState.create({
-      extensions: [basicSetup, oneDark, StreamLanguage.define(haskell)]
+      extensions: [basicSetup, oneDark, StreamLanguage.define(haskell), keymap.of(commands)]
     }),
     parent: document.body
   })
