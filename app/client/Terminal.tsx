cc597d114a93fe6604cbcea2ee54ff8476054f77
Matthew <matthew@mindofmatthew.com>
2021-10-12 17:56:59 -0400

    Improve styling of terminal component

--- app/client/Terminal.tsx
+++ app/client/Terminal.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import clsx from "clsx";
 
 export interface TerminalMessage {
@@ -11,9 +12,21 @@ interface TerminalProps {
 }
 
 export function Terminal({ feed }: TerminalProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollTo({
+        top: ref.current.scrollHeight - ref.current.clientHeight,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  }, [feed]);
+
   return (
     <section id="terminal">
-      <div id="terminal-contents">
+      <div id="terminal-contents" ref={ref}>
         {feed.map((message) => (
           <TerminalItem message={message} />
         ))}
@@ -28,10 +41,28 @@ interface TerminalItemProps {
 
 function TerminalItem({ message }: TerminalItemProps) {
   let { level, source, text } = message;
+
+  let lines = text.split("\n").map((lineText, i) => {
+    let [, indent, line] = lineText.match(/^(\s*)(.*)/) ?? [];
+    let tabLevel = Math.floor(indent.length / 4);
+    return (
+      <div
+        key={i}
+        style={{ paddingLeft: `${tabLevel + 2}ch`, textIndent: "-2ch" }}
+      >
+        {line
+          .split("/")
+          .flatMap((part, i, arr) =>
+            i < arr.length - 1 ? [part, "/", <wbr />] : [part]
+          )}
+      </div>
+    );
+  });
+
   return (
     <div className={clsx("item", level)}>
       <div className="source">{source}</div>
-      {text}
+      {lines}
     </div>
   );
 }
--- app/server/ghci.ts
+++ app/server/ghci.ts
@@ -2,6 +2,8 @@ import { createSocket } from "dgram";
 import { spawn, ChildProcessWithoutNullStreams } from "child_process";
 import { createInterface } from "readline";
 
+import { performance } from "perf_hooks";
+
 import { message } from "../osc/osc";
 
 import bootCode from "bundle-text:./BootTidal.hs";
@@ -9,6 +11,9 @@ import bootCode from "bundle-text:./BootTidal.hs";
 export class GHCI {
   private socket;
   private process?: ChildProcessWithoutNullStreams;
+
+  private outBatch: string[] | null = null;
+  private errBatch: string[] | null = null;
 
   constructor(callback: (data: Buffer) => any) {
     this.socket = createSocket("udp4");
@@ -29,15 +34,43 @@ export class GHCI {
 
       out.on("line", (data) => {
         if (!data.endsWith("> ")) {
-          console.log(`GHCI: "${data}"`);
-          callback(Buffer.from(message("/tidal/reply", data)));
+          if (this.outBatch) {
+            this.outBatch.push(data);
+          } else {
+            this.outBatch = [data];
+
+            setTimeout(() => {
+              if (this.outBatch) {
+                const outBatch = this.outBatch;
+                this.outBatch = null;
+
+                callback(
+                  Buffer.from(message("/tidal/reply", outBatch.join("\n")))
+                );
+              }
+            }, 20);
+          }
         }
       });
 
       err.on("line", (data) => {
         if (data !== "") {
-          console.error(`Error: ${data}`);
-          callback(Buffer.from(message("/tidal/error", data)));
+          if (this.errBatch) {
+            this.errBatch.push(data);
+          } else {
+            this.errBatch = [data];
+
+            setTimeout(() => {
+              if (this.errBatch) {
+                const errBatch = this.errBatch;
+                this.errBatch = null;
+
+                callback(
+                  Buffer.from(message("/tidal/error", errBatch.join("\n")))
+                );
+              }
+            }, 20);
+          }
         }
       });
 
