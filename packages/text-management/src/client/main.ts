a97f8eb00ab42f7f99b9b9e68ba42772e8a86bdc
Matthew Kaney <matthew@mindofmatthew.com>
2022-12-31 11:44:55 -0800

    Rearrange the repository to simplify package setup
    Fixes #29

--- packages/text-management/src/client/main.ts
+++
@@ -1,8 +0,0 @@
-import { session } from "./currentSession";
-import { createEditor } from "./editor";
-
-window.addEventListener("load", () => {
-  const parent = document.body.appendChild(document.createElement("section"));
-  parent.id = "editor";
-  createEditor(session, parent);
-});
