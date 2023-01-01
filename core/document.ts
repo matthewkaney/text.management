58170d8e3029fa3a3c593b624d3fdaa6c26992c6
Matthew Kaney <matthew@mindofmatthew.com>
2023-01-01 11:37:34 -0800

    Add changed events to document class

--- core/document.ts
+++ core/document.ts
@@ -1,32 +1,60 @@
 import { ChangeSet, Text } from "@codemirror/state";
 
 import { readFile, writeFile } from "fs/promises";
+import { EventEmitter } from "./events";
 
-export class Document {
+interface DocumentEvents {
+  changed: boolean;
+}
+
+export class Document extends EventEmitter<DocumentEvents> {
   public static async create(path?: string) {
     if (path) {
       try {
-        return new Document(await readFile(path, { encoding: "utf-8" }), path);
+        let doc = await readFile(path, { encoding: "utf-8" });
+        return new Document(Text.of(doc.split(/\r?\n/)), path, false);
       } catch (err) {
-        if (err.code === "ENOENT") {
-          return new Document("", path);
+        if ((err as NodeJS.ErrnoException).code === "ENOENT") {
+          return new Document(Text.of([""]), path);
         } else {
           throw err;
         }
       }
     } else {
-      return new Document("");
+      return new Document(Text.of([""]));
     }
   }
 
   private path: string | undefined;
   private doc: Text;
 
+  private _changed = true;
+
+  get changed() {
+    return this._changed;
+  }
+
+  protected set changed(changed) {
+    this.emit("changed", changed);
+    this._changed = changed;
+  }
+
   private autosaveTime = 1000;
 
-  private constructor(docText: string, path?: string) {
+  private constructor(doc: Text, path?: string, changed: boolean = true) {
+    super();
+
+    this.onListener["changed"] = (listener) => {
+      listener(this.changed);
+    };
+
     this.path = path;
-    this.doc = Text.of(docText.split("\n"));
+    this.doc = doc;
+    this.changed = changed;
+
+    if (this.changed) {
+      this.save();
+    }
   }
 
   get contents() {
@@ -51,6 +79,10 @@ export class Document {
   update(changes: ChangeSet) {
     this.doc = changes.apply(this.doc);
 
+    if (!this.changed) {
+      this.changed = true;
+    }
+
     if (this.autosaveTimer) {
       clearTimeout(this.autosaveTimer);
       this.autosaveTimer = undefined;
@@ -65,13 +97,17 @@ export class Document {
   private writing = false;
   private writeRequest = false;
 
-  private save() {
+  save() {
     const write = async () => {
       while (this.path && this.writeRequest) {
         this.writing = true;
         this.writeRequest = false;
         await writeFile(this.path, this.contents);
         this.writing = false;
+      }
+
+      if (this.changed) {
+        this.changed = false;
       }
     };
 
@@ -81,5 +117,9 @@ export class Document {
       this.writeRequest = true;
       write();
     }
+  }
+
+  saveAs(path: string) {
+    return new Document(this.doc, path);
   }
 }
