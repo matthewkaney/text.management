0c04cab763b559b84d59df4ff8cf492a62176547
Matthew Kaney <matthew@mindofmatthew.com>
2023-10-21 17:34:07 -0400

    Continuing playing with JSON Schema types

--- core/extensions/settings/schema.ts
+++ core/extensions/settings/schema.ts
@@ -1,13 +1,8 @@
-import { JSONSchema7, FromSchema as FromJSONSchema } from "json-schema-to-ts";
-import { Draft07 } from "json-schema-library";
+import { JSONSchema7, FromSchema } from "json-schema-to-ts";
 
 export interface SettingsSchema {
   [name: string]: JSONSchema7;
 }
-
-export type FromSchema<S extends SettingsSchema> = {
-  [Name in keyof S]: FromJSONSchema<S[Name]>;
-};
 
 export function asJSONSchema(schema: SettingsSchema) {
   return {
@@ -16,59 +11,33 @@ export function asJSONSchema(schema: SettingsSchema) {
   } as const;
 }
 
-export function getSettings<S extends SettingsSchema>(
+export function getSettings<S extends JSONSchema7>(
   schema: S,
   data: unknown
-) {
-  let settings: { [name in keyof S]: S[name] } = {};
+): FromSchema<S> {
+  let settings: { [name in keyof S]?: S[name] } = {};
 
-  for (let name in schema) {
+  for (let name in (schema as any).properties) {
     if (typeof data === "object" && data && name in data) {
-      settings[name] = data[name as keyof data];
+      settings[name] = (data as any)[name];
+    } else {
+      let settingSchema = schema[name];
+
+      if (typeof settingSchema === "boolean") {
+        throw Error("Unexpected boolean JSON schema");
+      }
+
+      let value = settingSchema.default;
+
+      if (value === undefined) {
+        if (settingSchema.type === "array") {
+          value = [];
+        }
+      }
+
+      settings[name] = value as any;
     }
   }
 
-  return;
+  return settings as any;
 }
-
-// function normalizeSettings<S extends JSONSchema>(
-//   typedSchema: S,
-//   settings: unknown
-// ) {
-//   let completeSettings: any = {};
-
-//   let schema = typedSchema as any;
-
-//   for (const [propName, propSchema] of Object.entries(schema.properties)) {
-//     // This shouldn't be necessary if schema has a specific type
-//     if (typeof propSchema !== "object" || propSchema === null) break;
-
-//     if (typeof settings === "object" && settings && propName in settings) {
-//       // TODO: Validate
-
-//       completeSettings[propName] = (settings as { [key: string]: unknown })[
-//         propName
-//       ];
-
-//       continue;
-//     }
-
-//     if ("const" in propSchema) {
-//       completeSettings[propName] = propSchema.const;
-//     } else if ("default" in propSchema) {
-//       completeSettings[propName] = propSchema.default;
-//     } else if ("type" in propSchema && propSchema.type === "array") {
-//       completeSettings[propName] = [];
-//     } else {
-//       throw Error(
-//         `No way to generate a default value for settings property "${propName}"`
-//       );
-//     }
-//   }
-
-//   return completeSettings as Required<FromSchema<S>>;
-// }
-
-// export function normalizeTidalSettings(settings: unknown) {
-//   return normalizeSettings(TidalSettingsSchema, settings);
-// }
--- packages/languages/tidal/settings.ts
+++ packages/languages/tidal/settings.ts
@@ -1,10 +1,13 @@
-import { SettingsSchema, FromSchema } from "@core/extensions/settings/schema";
+import { JSONSchema7, FromSchema } from "json-schema-to-ts";
 
 export const TidalSettingsSchema = {
-  // environment: { const: "ghci" },
-  "tidal.boot.useDefaultFile": { type: "boolean", default: true },
-  "tidal.boot.customFiles": { type: "array", items: { type: "string" } },
-  "tidal.boot.disableEditorIntegration": { type: "boolean", default: false },
-} satisfies SettingsSchema;
+  type: "object",
+  properties: {
+    // environment: { const: "ghci" },
+    "tidal.boot.useDefaultFile": { type: "boolean", default: true },
+    "tidal.boot.customFiles": { type: "array", items: { type: "string" } },
+    "tidal.boot.disableEditorIntegration": { type: "boolean", default: false },
+  },
+} satisfies JSONSchema7;
 
 export type TidalSettings = FromSchema<typeof TidalSettingsSchema>;
