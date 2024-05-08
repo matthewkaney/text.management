c19c201ede5fbb1f02033bcbcfd50c121a7f305a
Matthew Kaney <matthew@mindofmatthew.com>
2024-05-08 10:44:07 -0400

    Dependency management

--- core/api.ts
+++ core/api.ts
@@ -1,29 +1,8 @@
-import { BehaviorSubject, Observable } from "rxjs";
-
-import { Text } from "@codemirror/state";
-
 export interface DocumentUpdate {
   version: number;
   clientID: string;
   changes: any;
   evaluations?: ([number, number] | [string])[];
-}
-
-export interface Document {
-  initialVersion: number;
-  initialText: Text;
-  updates$: Observable<DocumentUpdate>;
-  pushUpdate(update: DocumentUpdate): Promise<boolean>;
-}
-
-export interface FileDocument extends Document {
-  saveState$: Observable<boolean>;
-  path: string | null;
-}
-
-export interface Tab {
-  name$: BehaviorSubject<string>;
-  content: Promise<Document>;
 }
 
 export interface Evaluation {
