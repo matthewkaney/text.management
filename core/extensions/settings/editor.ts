3182d0e29801f8ca1c6d80273f97a7bc2a9692e4
Matthew Kaney <matthew@mindofmatthew.com>
2024-05-14 18:57:23 -0400

    Overhaul settings system to support color schemes

--- core/extensions/settings/editor.ts
+++ core/extensions/settings/editor.ts
@@ -2,13 +2,18 @@ import { autocompletion } from "@codemirror/autocomplete";
 import { json } from "@codemirror/lang-json";
 import { jsonSchema } from "codemirror-json-schema";
 
+import { ThemeSettingsSchema } from "@core/extensions/theme/settings";
 import { TidalSettingsSchema } from "packages/languages/tidal/settings";
 
 export function settings() {
   return [
     autocompletion(),
     json(),
-    // TODO: Figure out how to get all the JSON Schema extensions to work together
-    jsonSchema(TidalSettingsSchema),
+    jsonSchema({
+      properties: {
+        ...ThemeSettingsSchema.properties,
+        ...TidalSettingsSchema.properties,
+      },
+    }),
   ];
 }
--- packages/languages/tidal/ghci.ts
+++ packages/languages/tidal/ghci.ts
@@ -8,7 +8,7 @@ import { parse } from "@core/osc/osc";
 import { Evaluation, Log } from "@core/api";
 import { Engine } from "../core/engine";
 
-import { StateManagement } from "@core/state";
+import { Config, ConfigExtension } from "@core/state";
 export { TidalSettingsSchema } from "./settings";
 import { TidalSettingsSchema } from "./settings";
 
@@ -33,13 +33,17 @@ interface GHCIEvents {
 }
 
 export class GHCI extends Engine<GHCIEvents> {
+  private settings: ConfigExtension<typeof TidalSettingsSchema>;
+
   private socket: Promise<Socket>;
   private process: Promise<ChildProcessWithoutNullStreams>;
 
   private history: (Evaluation | Log)[] = [];
 
-  constructor(private settings: StateManagement<typeof TidalSettingsSchema>) {
+  constructor(settings: Config) {
     super();
+
+    this.settings = settings.extend(TidalSettingsSchema);
 
     this.settings.on("change", () => {
       this.reloadSettings;
@@ -94,12 +98,11 @@ export class GHCI extends Engine<GHCIEvents> {
   private wrapper: ProcessWrapper | null = null;
 
   private async initProcess() {
-    console.log(JSON.stringify(this.settings.getData()));
     const {
       "tidal.boot.disableEditorIntegration": disableEditorIntegration,
       "tidal.boot.useDefaultFile": useDefaultBootfile,
       "tidal.boot.customFiles": bootFiles,
-    } = this.settings.getData();
+    } = this.settings.data;
     const port = (await this.socket).address().port.toString();
 
     // this.outputFilters.push(
