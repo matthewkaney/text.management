2fd1a67a8fbd017489c293dc972083603bc906fe
Matthew Kaney <matthew@mindofmatthew.com>
2024-05-19 12:56:32 -0400

    Move to Preact to support dynamic clipping around console message source label

--- core/extensions/console/message.tsx
+++ core/extensions/console/message.tsx
@@ -1,5 +1,5 @@
-import { render } from "nano-jsx";
-import { clsx } from "clsx/lite";
+import { render } from "preact";
+import { useCallback } from "preact/hooks";
 
 import { Evaluation, Log } from "@core/api";
 
@@ -19,12 +19,53 @@ type TerminalMessage = {
 export function messageConstructor(rawMessage: Evaluation | Log) {
   let message = format(rawMessage);
 
-  return render(
-    <div
-      class={clsx("cm-console-message", `cm-console-message-${message.level}`)}
-    >
+  let messageDom = document.createElement("div");
+  messageDom.classList.add(
+    "cm-console-message",
+    `cm-console-message-${message.level}`
+  );
+
+  render(<MessageContent message={message} />, messageDom);
+
+  return messageDom;
+}
+
+interface MessageContentProps {
+  message: TerminalMessage;
+}
+
+function MessageContent({ message }: MessageContentProps) {
+  const ref = useCallback((current: HTMLDivElement | null) => {
+    console.log(current);
+    if (current !== null) {
+      const observer = new ResizeObserver(([size]) => {
+        let { blockSize, inlineSize } = size.borderBoxSize[0];
+        console.log(size);
+        const points = [
+          "0 0",
+          `calc(100% - ${inlineSize + 1}px) 0`,
+          `calc(100% - ${inlineSize + 1}px) ${blockSize + 1}px`,
+          `100% ${blockSize + 1}px`,
+          "100% 100%",
+          "0 100%",
+        ];
+        console.log(points.join(", "));
+        current.style.clipPath = `polygon(${points.join(", ")})`;
+      });
+
+      let source = current.parentElement?.querySelector(
+        ".cm-console-message-source"
+      );
+      if (source) {
+        observer.observe(source);
+      }
+    }
+  }, []);
+
+  return (
+    <>
       <div class="cm-console-message-source">{message.source}</div>
-      <div class="cm-console-message-content">
+      <div class="cm-console-message-content" ref={ref}>
         {"input" in message && (
           <div class="cm-console-message-input">
             <InputIcon />
@@ -38,7 +79,7 @@ export function messageConstructor(rawMessage: Evaluation | Log) {
           </div>
         )}
       </div>
-    </div>
+    </>
   );
 }
 
