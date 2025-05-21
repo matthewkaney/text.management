8ecd149292a64e9633094db2f3b4225c9aa60b71
Matthew Kaney <matthew@mindofmatthew.com>
2025-05-21 10:21:54 +0100

    Get highlighting and editor integrations working with 1.10

--- packages/languages/tidal/editor-integration/index.ts
+++ packages/languages/tidal/editor-integration/index.ts
@@ -3,9 +3,8 @@ import satisfies from "semver/functions/satisfies";
 import v1_9_2Hosc from "bundle-text:./Hosc-1.9.2.ghci";
 import v1_9_3Hosc from "bundle-text:./Hosc-1.9.3.ghci";
 import editorPort from "bundle-text:./EditorPort.ghci";
-import streamGetNow from "bundle-text:./StreamGetNow-1.9.x.ghci";
-import integrations from "bundle-text:./Integrations.ghci";
-import v1_9Hacks from "bundle-text:./1.9.x-Hacks.ghci";
+import v1_9_integrations from "bundle-text:./Integrations_1.9.ghci";
+import v1_10_integrations from "bundle-text:./Integrations_1.10.ghci";
 
 export function generateIntegrationCode(version: string) {
   let integrationCode = "";
@@ -21,13 +20,9 @@ export function generateIntegrationCode(version: string) {
   integrationCode = integrationCode.concat("\n", editorPort);
 
   if (satisfies(version, "<1.10.0")) {
-    integrationCode = integrationCode.concat("\n", streamGetNow);
-  }
-
-  integrationCode = integrationCode.concat("\n", integrations);
-
-  if (satisfies(version, "<1.10.0")) {
-    integrationCode = integrationCode.concat("\n", v1_9Hacks);
+    integrationCode = integrationCode.concat("\n", v1_9_integrations);
+  } else {
+    integrationCode = integrationCode.concat("\n", v1_10_integrations);
   }
 
   return integrationCode;
